5e5e9af2c701781e503fa2d2f892a827e0a3f76d
mon <mon.ppunnavich@gmail.com>
2023-12-03 05:08:01 +0700

    update uncommit file

---
+++ web_app/backend/pillmate/src/test/java/com/example/pillmate/PillmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pillmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
