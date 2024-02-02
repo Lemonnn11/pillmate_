1b7053dc6d5be1bd1fe9a26461a477b407f581f4
Lemonnn11 <82075408+Lemonnn11@users.noreply.github.com>
2024-02-02 15:39:46 +0700

    Delete mobile_app/pillmate directory

--- mobile_app/pillmate/android/app/src/main/kotlin/com/example/pillmate/pillmate/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pillmate.pillmate
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mobile_app/pillmate/lib/constants/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:ui';
-
-
-const kLightGreen = Color(0xff75DB6E);
-const kTeal = Color(0xff27B791);
-const GOOGLE_MAPS_API_KEY = "AIzaSyDBq1_J47STSxQY5RsV9X4sWHS6R2NC7gM";
