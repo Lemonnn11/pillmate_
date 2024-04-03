0aa34073a2a0595a38f0bcd10d3fafec2740aaa3
mon <mon.ppunnavich@gmail.com>
2024-04-03 21:45:36 +0700

    init map mock(http)

--- mobile_app/pillmate/test/map_test.dart
+++ mobile_app/pillmate/test/map_test.dart
@@ -1,7 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:pillmate/services/map.dart';
+import 'package:http/http.dart' as http;
 
-@GenerateNiceMocks([MockSpec<MapService>()])
+import 'map_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<MapService>(), MockSpec<http.Client>()])
 void main(){
+  late MapService mapService;
+  setUpAll((){
+    mapService = MockMapService();
+  });
 
+  group('test get current location', () {
+    test('get current location on mobile phone', (){
+
+    });
+  });
 }
